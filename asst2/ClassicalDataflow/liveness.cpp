45592b8828bd9050b041b535a247af2ac9d8c43e
nipunn1313 <nkoorapa@andrew.cmu.edu>
2013-02-10 20:28:07 -0500

    Finished up the implementation for liveness

--- asst2/ClassicalDataflow/liveness.cpp
+++ asst2/ClassicalDataflow/liveness.cpp
@@ -9,6 +9,8 @@
 
 #include "dataflow.h"
 
+#include <iostream>
+
 using namespace llvm;
 
 namespace {
@@ -23,10 +25,40 @@ class Liveness : public FunctionPass {
 
     public:
       LivenessDF(std::map<Value*, int>* map) :
-        DataFlow(emptySet(map->size()), emptySet(map->size()), BACKWARDS) {}
+        DataFlow(emptySet(map->size()), emptySet(map->size()), BACKWARDS) {
+          this->map = map;
+
+#if 0
+          for (std::map<Value*, int>::iterator it = map->begin(), end =
+              map->end(); it != end; ++it) {
+            fprintf(stderr, "%p -> %d\n", it->first, it->second);
+            it->first->print(errs());
+            fprintf(stderr, "\n");
+          }
+#endif
+      }
       ~LivenessDF() {}
 
       virtual BitVector transferFunction(Instruction* inst, BitVector before) {
+
+        std::map<Value*, int>::iterator it;
+
+        // Eliminate kill set (writing to a variable)
+        if (StoreInst* storeInst = dyn_cast<StoreInst>(inst)) {
+          Value* v = storeInst->getPointerOperand();
+          it = map->find(v);
+          if (it != map->end()) {
+            before[it->second] = false;
+          }
+        }
+        // Union with vars_used set
+        else if (LoadInst* loadInst = dyn_cast<LoadInst>(inst)) {
+          it = map->find(loadInst->getPointerOperand());
+          if (it != map->end()) {
+            before[it->second] = true;
+          }
+        }
+
         // (before - kill) U vars_used
         return before;
       }
@@ -44,6 +76,59 @@ class Liveness : public FunctionPass {
     // Make one pass through to find all variables.
     std::vector<Value*> varList;
     std::map<Value*, int> varMap;
+    BasicBlock& entryBlock = F.getEntryBlock();
+
+    // Walk through entry block to find all variable candidates
+    for (BasicBlock::iterator iter = entryBlock.begin(),
+         end = entryBlock.end(); iter != end; ++iter) {
+      Instruction* inst = &(*iter);
+      if (dyn_cast<AllocaInst>(inst)) {
+        varMap[inst] = 0;
+      }
+    }
+    // Walk through all instructions to make sure variables are valid.
+    for (inst_iterator iter = inst_begin(F), end = inst_end(F);
+         iter != end; ++iter) {
+      Instruction* inst = &(*iter);
+      if (dyn_cast<AllocaInst>(inst)) {
+        // Do nothing
+      } else if (StoreInst* storeInst = dyn_cast<StoreInst>(inst)) {
+        Value* p = storeInst->getPointerOperand();
+        Value* v = storeInst->getValueOperand();
+        std::map<Value*, int>::iterator it = varMap.find(v);
+        if (it != varMap.end()) {
+          varMap.erase(it);
+        }
+        it = varMap.find(p);
+        if (it != varMap.end() && storeInst->isVolatile()) {
+          varMap.erase(it);
+        }
+      } else if (LoadInst* loadInst = dyn_cast<LoadInst>(inst)) {
+        Value* v = loadInst->getPointerOperand();
+        std::map<Value*, int>::iterator it = varMap.find(v);
+        if (it != varMap.end() && loadInst->isVolatile()) {
+          varMap.erase(it);
+        }
+      } else {
+        for (User::value_op_iterator op = inst->value_op_begin(),
+             opEnd = inst->value_op_end(); op != opEnd; ++op) {
+          Value* v = *op;
+          std::map<Value*, int>::iterator it = varMap.find(v);
+          if (it != varMap.end()) {
+            varMap.erase(it);
+          }
+        }
+      }
+    }
+
+    // Assign indices to the variables in this function
+    int idx = 0;
+    for (std::map<Value*, int>::iterator iter = varMap.begin(),
+         end = varMap.end(); iter != end; ++iter) {
+      varList.push_back(iter->first);
+      iter->second = idx;
+      idx++;
+    }
 
     // Run analysis to get instruction -> bitmap
     LivenessDF ldf(&varMap);
