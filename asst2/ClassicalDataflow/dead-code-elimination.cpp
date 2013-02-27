6b6c10c495b8c123f4da11dada1861d0f5a259e1
user <user@s1315745.alias.cs.cmu.edu>
2013-02-26 22:02:45 -0500

    Changes for dead-code elimination

--- asst2/ClassicalDataflow/dead-code-elimination.cpp
+++ asst2/ClassicalDataflow/dead-code-elimination.cpp
@@ -1,5 +1,6 @@
 // 15-745 S13 Assignment 2: liveness.cpp
 // Group: nkoorapa, pdixit
+// TODO : Update this at end
 // Description : This file implements liveness analysis by exploiting the 
 //               generic framework in dataflow.h This also serves as an example
 //               of how to use the generic framework. 
@@ -33,6 +34,7 @@
 #include "llvm/Pass.h"
 #include "llvm/Support/InstIterator.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IntrinsicInst.h"
 
 #include "dataflow.h"
 
@@ -48,16 +50,17 @@ class Liveness : public FunctionPass {
 
   // Map from a variable to it's index in vector of variables
   typedef std::map<Value*, int> idxmap_t;
-  typedef std::pair<BitVector, BitVector> GKPair;
+  //typedef std::pair<BitVector, BitVector> GKPair;
+  typedef BitVector GenSet;
 
   /* Composeable parameters where parameters are a Gen/Kill pair */
-  class LivenessDF : public DataFlow<GKPair> {
+  class LivenessDF : public DataFlow<GenSet> {
     private:
       idxmap_t* map;
 
     public:
       LivenessDF(idxmap_t* map) :
-        DataFlow<GKPair>(emptySet(map->size()),
+        DataFlow<GenSet>(emptySet(map->size()),
                          emptySet(map->size()), BACKWARDS) {
           this->map = map;
 
@@ -73,46 +76,37 @@ class Liveness : public FunctionPass {
       ~LivenessDF() {}
 
       // Override. (in - kill) U gen
-      BitVector transferFunctionParams(GKPair params, BitVector input) {
-        return input.reset(params.second) |= params.first;
+      BitVector transferFunctionParams(GenSet params, BitVector input) {
+        return input |= params;
       }
 
       // Override
-      GKPair compose(GKPair a, GKPair b) {
-        // gen = (a.gen - b.kill) U b.gen
-        BitVector gen = a.first;
-        gen.reset(b.second) |= b.first;
-        // kill = a.kill U b.kill
-        BitVector kill = a.second;
-        kill |= b.second;
-        return GKPair::pair(gen, kill);
+      GenSet compose(GenSet a, GenSet b) {
+        return (a |= b);
       }
 
-      GKPair getTFParams(Instruction* inst) {
+      GenSet getTFParams(Instruction* inst) {
         idxmap_t::iterator it;
-        BitVector gen = emptySet(map->size());
-        BitVector kill = emptySet(map->size());
+        GenSet gen = emptySet(map->size());
 
         // Identity transfer function (on no instructions)
         if (inst == NULL) {
-          // empty gen/kill sets
+          // empty gen sets
         }
-        // kill set (writing to a variable)
-        else if (StoreInst* storeInst = dyn_cast<StoreInst>(inst)) {
-          it = map->find(storeInst->getPointerOperand());
-          if (it != map->end()) {
-            kill[it->second] = true;
-          }
-        }
-        // gen set (loading from variable)
-        else if (LoadInst* loadInst = dyn_cast<LoadInst>(inst)) {
-          it = map->find(loadInst->getPointerOperand());
-          if (it != map->end()) {
-            gen[it->second] = true;
-          }
-        }
+        // gen set (writing to a variable)
+        else {
+            if (isa<TerminatorInst>(inst) ||
+                isa<DbgInfoIntrinsic>(inst) ||
+                isa<LandingPadInst>(inst) ||
+                inst->mayHaveSideEffects()) {
 
-        return GKPair::pair(gen, kill);
+                it = map->find(inst);
+                if (it != map->end()) {
+                    gen[it->second] = true;
+                }
+            }
+        }
+        return gen;
       }
 
       // Override
@@ -126,87 +120,33 @@ class Liveness : public FunctionPass {
   virtual bool runOnFunction(Function& F) {
 
     // Make one pass through to find all variables.
-    std::vector<Value*> varList;
+    //std::vector<Value*> varList;
     idxmap_t varMap;
-    BasicBlock& entryBlock = F.getEntryBlock();
 
-    // Walk through entry block to find all variable candidates
-    for (BasicBlock::iterator iter = entryBlock.begin(),
-         end = entryBlock.end(); iter != end; ++iter) {
-      Instruction* inst = &(*iter);
-      if (dyn_cast<AllocaInst>(inst)) {
-        varMap[inst] = 0;
-      }
-    }
-    // Walk through all instructions to make sure variables are valid.
+    // Walk through all instructions to get variable candidates
     for (inst_iterator iter = inst_begin(F), end = inst_end(F);
          iter != end; ++iter) {
       Instruction* inst = &(*iter);
-      if (dyn_cast<AllocaInst>(inst)) {
-        // Do nothing
-      } else if (StoreInst* storeInst = dyn_cast<StoreInst>(inst)) {
-        Value* p = storeInst->getPointerOperand();
-        Value* v = storeInst->getValueOperand();
-        idxmap_t::iterator it = varMap.find(v);
-        if (it != varMap.end()) {
-          varMap.erase(it);
-        }
-        it = varMap.find(p);
-        if (it != varMap.end() && storeInst->isVolatile()) {
-          varMap.erase(it);
-        }
-      } else if (LoadInst* loadInst = dyn_cast<LoadInst>(inst)) {
-        Value* v = loadInst->getPointerOperand();
-        idxmap_t::iterator it = varMap.find(v);
-        if (it != varMap.end() && loadInst->isVolatile()) {
-          varMap.erase(it);
-        }
-      } else {
-        for (User::value_op_iterator op = inst->value_op_begin(),
-             opEnd = inst->value_op_end(); op != opEnd; ++op) {
-          Value* v = *op;
-          idxmap_t::iterator it = varMap.find(v);
-          if (it != varMap.end()) {
-            varMap.erase(it);
-          }
-        }
-      }
+      varMap[inst] = 0;
     }
 
-    // Assign indices to the variables in this function
-    int idx = 0;
-    for (idxmap_t::iterator iter = varMap.begin(),
-         end = varMap.end(); iter != end; ++iter) {
-      varList.push_back(iter->first);
-      iter->second = idx;
-      idx++;
-    }
 
     // Run analysis to get instruction -> bitmap
     LivenessDF ldf(&varMap);
     std::map<Instruction*, BitVector> result = ldf.doAnalysis(F);
 
-    // Output!
-    for (inst_iterator iter = inst_begin(F), end = inst_end(F);
-         iter != end; ++iter) {
-      Instruction* inst = &(*iter);
-      fprintf(stderr, "Live: {");
-      BitVector& bits = result[inst];
-      for (unsigned i=0; i<bits.size(); i++) {
-        if (bits[i]) {
-          Value* v = varList[i];
-          v->print(errs());
-          fprintf(stderr, "; ");
-        }
+    // Print out the dead instructions :
+    for (idxmap_t::iterator iter = varMap.begin(),
+         end = varMap.end(); iter != end; ++iter) {
+      if (varMap[iter->first] == 0) {
+        Value* v = iter->first;
+        v->print(errs());
+        fprintf (stderr, "\n");
       }
-      fprintf(stderr, "}\n");
-      inst->print(errs());
-      fprintf(stderr, "\n");
     }
-    fprintf(stderr, "Live: {}\n");
 
-    // Did not modify the incoming Function.
-    return false;
+    // the incoming Function is modified.
+    return true;
   }
 
   virtual void getAnalysisUsage(AnalysisUsage& AU) const {
@@ -217,6 +157,6 @@ class Liveness : public FunctionPass {
 };
 
 char Liveness::ID = 0;
-RegisterPass<Liveness> X("cd-liveness", "15745 Liveness");
+RegisterPass<Liveness> X("cd-dce", "15745 Dead-Code-Elimination");
 
 }
