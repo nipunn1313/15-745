dee8c2cf2fc6536121715a479fa7dfaefde40ee9
nipunn1313 <nkoorapa@andrew.cmu.edu>
2013-03-05 22:55:54 -0500

    Made couple of fixes to DCE
    1) Only update bit for instruction if ANY of the uses are live
    2) Return empty vector on NULL instruction
    
    Haven't tested because I don't have loop.o

--- asst2/ClassicalDataflow/dead-code-elimination.cpp
+++ asst2/ClassicalDataflow/dead-code-elimination.cpp
@@ -1,6 +1,6 @@
-// 15-745 S13 Assignment 2: liveness.cpp
+// 15-745 S13 Assignment 2: dead-code-elimination.cpp
 // Group: nkoorapa, pdixit
-// Description : This file implements liveness analysis by exploiting the   // TODO : Update this at end .. Also change 'Liveness' to something else
+// Description : This file implements dead-code-elimination analysis by exploiting the   // TODO : Update this at end .. Also change 'Liveness' to something else
 //               generic framework in dataflow.h This also serves as an example
 //               of how to use the generic framework. 
 //
@@ -11,9 +11,9 @@
 //                  is pair of gen and kill sets that can be associated to each
 //                  instruction or basic block
 //
-//               2. LivenessDF is the main dataflow class which extends DataFlow 
+//               2. DeadCodeEliminationDF is the main dataflow class which extends DataFlow 
 //                  with GKPair and uses constructor parameters to indicate that
-//                  Liveness is a BACKWARDS analysis with emptySet as the TOP 
+//                  DeadCodeElimination is a BACKWARDS analysis with emptySet as the TOP 
 //                  and boundary values.
 //          
 //               3. Virtual function transferFunctionParams is implemented to 
@@ -25,7 +25,8 @@
 //               5. Meet operator function is overriden to give union
 //
 //               6. getTFParams is overriden to return gen and kill sets for 
-//                  each instruction based on how they are defined for liveness
+//                  each instruction based on how they are defined for
+//                  dead-code-elimination
 // 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -43,34 +44,34 @@ using namespace llvm;
 
 namespace {
 
-class Liveness : public FunctionPass {
+class DeadCodeElimination : public FunctionPass {
  public:
   static char ID;
 
   // Map from a variable to it's index in vector of variables
   typedef std::map<Value*, int> idxmap_t;
   //typedef std::pair<BitVector, BitVector> GKPair;
-  
-  // For one instruction, it's Transfer-Function parameter are 
+
+  // For one instruction, it's Transfer-Function parameter are
   // it's index in the bit vector, and users
   // These can be computed and cached and used again and again
   typedef struct {
     bool definitely_live;
-    int index;  
+    int index;
     BitVector uses;
   } deadcode_params_inst_t;
 
-  // Generic Transfer-Function parameter for basic-blocks 
+  // Generic Transfer-Function parameter for basic-blocks
   // (one element per instr)
   typedef std::vector<deadcode_params_inst_t> deadcode_params_t;
 
   /* Composeable parameters where parameters are a Gen/Kill pair */
-  class LivenessDF : public DataFlow<deadcode_params_t> {
+  class DeadCodeEliminationDF : public DataFlow<deadcode_params_t> {
     private:
       idxmap_t* index_map;
 
     public:
-      LivenessDF(idxmap_t* index_map) :
+      DeadCodeEliminationDF(idxmap_t* index_map) :
         DataFlow<deadcode_params_t>(emptySet(index_map->size()),
                          emptySet(index_map->size()), BACKWARDS) {
           this->index_map = index_map;
@@ -84,22 +85,22 @@ class Liveness : public FunctionPass {
           }
 #endif
       }
-      ~LivenessDF() {}
+      ~DeadCodeEliminationDF() {}
 
       // Override. (in - kill) U gen
       // TODO : Make it take reference to params
       BitVector transferFunctionParams(deadcode_params_t params, BitVector input) {
         // Set input bits for all the uses which are already set:
-        for (deadcode_params_t::iterator iter = params.begin(), 
+        for (deadcode_params_t::iterator iter = params.begin(),
           end = params.end(); iter != end; ++iter) {
-          
+
           deadcode_params_inst_t* temp = &(*iter);
-          
+
           // If input is set for an index or it is definitely live,
           // set the use bits in input
-          if (temp->definitely_live || input[temp->index]) {
+          if (temp->definitely_live || input[temp->index] ||
+              input.anyCommon(temp->uses)) {
             input[temp->index] = true; // Ensures for definitely_live case
-            input |= temp->uses;
           }
         }
         return input;
@@ -121,58 +122,39 @@ class Liveness : public FunctionPass {
 
         // Value to return is a vector
         deadcode_params_t ret_result;
-        
-        // Initial values :
-        result.definitely_live = false;
-        result.index = 0;
-        result.uses = emptySet(index_map->size());
 
         // Identity transfer function (on no instructions)
         if (inst == NULL) {
-          // Empty
+          // Empty vector for no instructions
+          return ret_result;
         }
-        else {
-            // Definitely live case:
-            if (isa<TerminatorInst>(inst) ||
-                isa<DbgInfoIntrinsic>(inst) ||
-                isa<LandingPadInst>(inst) ||
-                inst->mayHaveSideEffects()) {
 
-                it = index_map->find(inst);
-                if (it != index_map->end()) {
-                    result.definitely_live = true;
-                    result.index = it->second;
-                    //*//fprintf (stderr, "Definitely live : ");
-                    //*//inst->print(errs());
-                    //*//fprintf (stderr, "\n");
-                }
-            }
+        // Initial values :
 
-            // Print the instruction :
-            //*//fprintf (stderr, "Instruction : ");
-            //*//inst->print(errs());
-            //*//fprintf (stderr, "\n");
+        // Definitely live case:
+        if (isa<TerminatorInst>(inst) ||
+            isa<DbgInfoIntrinsic>(inst) ||
+            isa<LandingPadInst>(inst) ||
+            inst->mayHaveSideEffects()) {
+          result.definitely_live = true;
+        } else {
+          result.definitely_live = false;
+        }
 
-            // Get the uses :
-            for (User::value_op_iterator iter = inst->value_op_begin(), 
-              end = inst->value_op_end(); iter != end; ++iter) {
-              
-              Value* use_inst =  *iter;
-              it = index_map->find(use_inst);
-            
-              //*//fprintf (stderr, "uses : ");
-              if (it != index_map->end()) {
-                
-                // Print the use instruction
-                //*//use_inst->print(errs());
+        result.index = (*index_map)[inst];
 
-                result.uses[it->second] = true;
-              }
-              //*//fprintf (stderr, "\n");
-            }
+        // Print the instruction :
+        //*//fprintf (stderr, "Instruction : ");
+        //*//inst->print(errs());
+        //*//fprintf (stderr, "\n");
 
+        // Get the uses :
+        result.uses = emptySet(index_map->size());
+        for (User::value_op_iterator iter = inst->value_op_begin(),
+            end = inst->value_op_end(); iter != end; ++iter) {
 
-
+          Value* use_inst = *iter;
+          result.uses[(*index_map)[use_inst]] = true;
         }
 
         // Push on vector and return it:
@@ -186,7 +168,7 @@ class Liveness : public FunctionPass {
       }
   };
 
-  Liveness() : FunctionPass(ID) { }
+  DeadCodeElimination() : FunctionPass(ID) { }
 
   virtual bool runOnFunction(Function& F) {
 
@@ -208,7 +190,7 @@ class Liveness : public FunctionPass {
 
 
     // Run analysis to get instruction -> bitmap
-    LivenessDF ldf(&varMap);
+    DeadCodeEliminationDF ldf(&varMap);
     std::map<Instruction*, BitVector> result = ldf.doAnalysis(F);
 
     // Print out the dead instructions :
@@ -220,7 +202,7 @@ class Liveness : public FunctionPass {
 
     for (unsigned i = 0; i < live_insts.size(); ++i) {
         if (live_insts[i] == false) {
-          varList[i]->print(errs());  
+          varList[i]->print(errs());
           errs()<< "\n";
         }
     }
@@ -236,7 +218,7 @@ class Liveness : public FunctionPass {
  private:
 };
 
-char Liveness::ID = 0;
-RegisterPass<Liveness> X("cd-dce", "15745 Dead-Code-Elimination");
+char DeadCodeElimination::ID = 0;
+RegisterPass<DeadCodeElimination> X("cd-dce", "15745 Dead-Code-Elimination");
 
 }
