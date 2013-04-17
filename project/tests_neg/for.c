45bb9d65cebb5ce2c574593a63ffafbec3b9a4c9
nipunn1313 <nkoorapa@andrew.cmu.edu>
2013-04-17 14:37:21 -0400

    Organize tests directory.

--- project/tests_neg/for.c
+++ project/tests_neg/for.c
@@ -4,26 +4,14 @@
 #define MAX_SIZE 1000000
 
 int main() {
-//    int a[MAX_SIZE];
-//    int b[MAX_SIZE];
-//    int c[MAX_SIZE];
     int *a = (int *) malloc (4 * MAX_SIZE);
     int *b = (int *) malloc (4 * MAX_SIZE);
     int *c = (int *) malloc (4 * MAX_SIZE);
 
     int i;
 
-//    for (i = 0; i < MAX_SIZE; i++) {
-//        a[i] = i;
-//        b[i] = 2 * i;
-//        c[i] = 0;
-//    }
-
     for (i = 0; i < MAX_SIZE; i++) {
-        //a[i] = a[i] + 1;
-        //b[i] = b[i] - 2;
         c[i] = a[i] + b[i];
-        //i++;
     };
     return 0;
 }
--- project/tests_pos/dowhile.c
+++ project/tests_pos/dowhile.c
@@ -4,26 +4,14 @@
 #define MAX_SIZE 1000000
 
 int main() {
-//    int a[MAX_SIZE];
-//    int b[MAX_SIZE];
-//    int c[MAX_SIZE];
     int *a = (int *) malloc (4 * MAX_SIZE);
     int *b = (int *) malloc (4 * MAX_SIZE);
     int *c = (int *) malloc (4 * MAX_SIZE);
 
     int i;
 
-//    for (i = 0; i < MAX_SIZE; i++) {
-//        a[i] = i;
-//        b[i] = 2 * i;
-//        c[i] = 0;
-//    }
-
     i = 0;
     do {
-//        a[i] = 3 + a[i];
-//        b[i] = b[i] - 2;
-//        a[i] = a[i] + b[i];
         c[i] = a[i] + b[i];
         i++;
     } while ( i < MAX_SIZE);
